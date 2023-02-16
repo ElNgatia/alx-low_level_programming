9f5535fd4e5a98c8e626d7aaad238f5ca94a8c7c
ElNgatia <ngatiadyte@gmail.com>
2023-02-17 01:50:22 +0300

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Program that prints a line using puts function
+ * return 0 - success
+ */
 int main(void)
 {
-	char MyStr[] = "Programming is like building a multilingual puzzle";
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyStr);
 
