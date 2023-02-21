827fc61ada2e9b291391200e50cb6b0a626b09d8
ElNgatia <ngatiadyte@gmail.com>
2023-02-22 01:20:49 +0300

    0._putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,8 @@
 
 /**
  * main - print _putchar
- * Return : Always 0
+ * written by Ngatia
+ * Return: Always 0
  */
 
 int main(void)
@@ -16,5 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
