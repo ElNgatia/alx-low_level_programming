7635f4b67f038800e873ba2bf61c4f9976a0f0d2
ElNgatia <ngatiadyte@gmail.com>
2023-03-06 20:11:31 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,17 @@
  * @s: area to be filled
  * @b: constant byte to fill
  * @n: number of byte to fill
+ * Return: changed array
  */
-char *_memset(char *s, char b, unsigned int n) {
-    char *ptr = s;
-    while (n > 0) {
-        *ptr = b;
-        ptr++;
-        n--;
-    }
-    return s;
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
+	return (s);
 }
